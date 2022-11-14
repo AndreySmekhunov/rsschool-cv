9a9831483ce95a9304985013f19f940bc9bfd7f7
AndreySmekhunov <smekhunov@gmail.com>
2022-11-14 18:37:24 +0300

    feat: add animation

--- songbird/script.js
+++ songbird/script.js
@@ -1,23 +1,50 @@
 
 import birdsData from './birds.js';
-let list = document.getElementsByClassName('birdList')[0];
+let list = document.getElementById('birdList');
+
 for (let i = 0; i < birdsData.length; i++) {
-    for (let j = 0; j < birdsData[i].length; j++) {
         let el = document.createElement('div');
+        el.id = i.toString();
         el.className = 'oneBird';
         let picture = document.createElement('div');
         picture.className = 'smallPhoto';
-        picture.style.backgroundImage  = 'url("' + birdsData[i][j].image + '")';
-        el.append(picture);
+        picture.style.backgroundImage  = 'url("' + birdsData[i].image + '")';
+
         let name = document.createElement('div');
         name.className = 'smallText';
-        let p = document.createElement('div');
-        p.textContent = birdsData[i][j].name;
-        name.append(p);
-        p = document.createElement('div');
-        p.textContent = birdsData[i][j].species;
-        name.append(p);
+        let rus = document.createElement('div');
+        rus.textContent = birdsData[i].name;
+        rus.className = 'rusName';
+        name.append(rus);
+        let lat = document.createElement('div');
+        lat.className = 'species';
+        lat.textContent = birdsData[i].species;
+        name.append(lat);
+        el.append(picture);
         el.append(name);
         list.append(el);
-    }
+}
+document.getElementById('birdList').addEventListener('click', showBird);
+
+function showBird(e) {
+    console.log('yes');
+    let target = e.target;
+    console.log(target);
+    // e.stopPropagation();
+    if (target.id == 'birdBox') return;
+    let num = Number(target.parentNode.id);
+    console.log(num);
+    let birdBox = document.createElement('div');
+    birdBox.className = 'birdBox';
+    
+    let photo = document.createElement('div');
+    photo.className = 'bigPhoto';
+    photo.style.backgroundImage = 'url("' + birdsData[num].image + '")';
+    let info = document.createElement('div');
+    info.className = 'infoBird';
+    info.textContent = birdsData[num].description;
+    birdBox.append(photo);
+    birdBox.append(info);
+    body.append(birdBox);
+
 }
