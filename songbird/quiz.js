7470c9ba98c99773abf9e78f20144cf8cc0434cc
AndreySmekhunov <smekhunov@gmail.com>
2022-11-18 17:04:51 +0300

    feat: repeat push

--- songbird/quiz.js
+++ songbird/quiz.js
@@ -1,9 +1,18 @@
+'use strict';
 import birdsData from './birds.js';
-let wrongAnswers = [];
+
 let l = 6;
+const yesOrNo = document.getElementById('yesOrNo');
+let audioQuiz = document.getElementById('audioQuiz');
+let audioDescription = document.getElementById('audioDescription');
+audioQuiz.isPlay = false;
+audioQuiz.playerName = 'playerQuiz';
+audioDescription.isPlay = false;
+audioDescription.playerName = 'playerDescription';
 let currentVariants = [];
 let count = 0;
 let score = 0;
+let wrongAnswers = [];
 let rnd = birdsData.sort(function(){return Math.random() - 0.5;})
 let answers = rnd.slice(0,l);
 let variants = rnd.slice(l);
@@ -14,27 +23,54 @@ for (let i = 0; i < 6; i++) {
     el.id = i.toString();
     field.append(el);
 }
+let inviteText = document.getElementsByClassName('inviteText')[0];
+let bigPhoto = document.getElementById('bigPhoto');
+let playerQuiz = document.getElementById('playerQuiz');
+let playerDescription = document.getElementById('playerDescription');
+let name = document.getElementsByClassName('name')[0];
+let hint = document.getElementsByClassName('hint')[0];
+let description = document.getElementById('description');
 let next = document.getElementsByClassName('longButton')[0];
 let currentQuestion = document.getElementsByClassName('questionTxt')[0];
 let scoreTxt = document.getElementsByClassName('score')[0];
 let progressBar = document.getElementsByClassName('oneStep')[0];
+let smallPhoto = document.getElementById('small');
 fillVariants();
-field.addEventListener('click', getAnswer);
 
+field.addEventListener('click', getAnswer);
+playerQuiz.addEventListener('click',() => play(audioQuiz));
+audioQuiz.addEventListener('ended',() => endPlay(audioQuiz));
+playerDescription.addEventListener('click',() => play(audioDescription));
+audioDescription.addEventListener('ended',() => endPlay(audioDescription));
     
 
+function initGame () {
+progressBar.style.width = 0;
+currentVariants = [];
+count = 0;
+score = 0;
+wrongAnswers = [];
+rnd = birdsData.sort(function(){return Math.random() - 0.5;})
+answers = rnd.slice(0,l);
+variants = rnd.slice(l);
+}
+
 function fillVariants() {
-progressBar.style.width = (100/l * count).toString() + '%';
-currentVariants = variants.slice(0,5);
+currentVariants = variants.sort(function(){return Math.random() - 0.5;}).slice(0,5);
 currentVariants.push(answers[count]);
 currentVariants.sort(function(){return Math.random() - 0.5;})
-let smallPhoto = document.getElementById('small');
+inviteText.style.display = 'block';
+bigPhoto.style.display = 'none';
+playerDescription.style.display = 'none';
+name.style.display = 'none';
+description.style.display = 'none';
 smallPhoto.style.backgroundImage = 'url("bird.svg")';
-currentQuestion.textContent = 'Вопрос ' + (count + 1) + ' из ' + l + answers[count].name;
+audioQuiz.src = answers[count].audio;
+currentQuestion.textContent = '*  *  *';
+hint.textContent = 'подсказка для тестеров: ' + answers[count].name;
 scoreTxt.textContent = 'Баллы: ' + score;
 for (let i = 0; i < 6; i++) {
     let el = document.getElementById(i.toString());
-    console.log(el);
     el.style.fontSize = '1.5em';
     el.style.color = 'white'; 
     el.style.cursor = 'pointer';
@@ -46,39 +82,120 @@ function getAnswer(e) {
  let target = e.target;
  let num = Number(target.id);
  if (isNaN(num))  return;
- console.log(num);
  if (wrongAnswers.indexOf(currentVariants[num].id) > -1) return;
-let el = document.getElementById((num).toString())
+let el = document.getElementById((num).toString());
+audioDescription.src = currentVariants[num].audio;
+inviteText.style.display = 'none';
+bigPhoto.style.display = 'block';
+playerDescription.style.display = 'block';
+name.style.display = 'block';
+description.style.display = 'block';
+bigPhoto.style.backgroundImage = 'url("' + currentVariants[num].image +'")';
+name.textContent = currentVariants[num].name + '\n' + currentVariants[num].species;
+description.textContent = currentVariants[num].description;
+
+
 if (currentVariants[num].id == [answers[count].id]) {
+    el.style.color = 'green'; 
+    yesOrNo.src = 'yes.mp3';
+    if (audioQuiz.isPlay) play(audioQuiz);
+    endPlay(audioDescription);
+    if (audioDescription.isPlay) play(audioDescription);
+    yesOrNo.play();
+    smallPhoto.style.backgroundImage = 'url("' + answers[count].image +'")';
+    currentQuestion.textContent = answers[count].name;
     count += 1;
-    el.style.fontSize = '2em'; 
+    progressBar.style.width = (100/l * count).toString() + '%';
     field.removeEventListener('click', getAnswer);
     score += 5 - wrongAnswers.length;
     wrongAnswers = [];
     scoreTxt.textContent = 'Баллы: ' + score;
     if (count == l) {
         progressBar.style.width = '100%';
-        showResult();
+        setTimeout(showResult,500);
     }
     else {
         next.addEventListener('click', nextQuestion);
-        next.style.backgroundColor = '#00a0a0'
+        next.style.backgroundColor = '#00a0a0';
+        next.style.cursor = 'pointer';
     }
 }
 else {
-    wrongAnswers.push(currentVariants.id);
+    yesOrNo.src = 'no.mp3';
+    yesOrNo.play();
+    wrongAnswers.push(currentVariants[num].id);
     el.style.color = 'red'; 
     el.style.cursor = 'default';
 }
 }
 
 function nextQuestion() {
+    if (audioQuiz.isPlay) {
+        play(audioQuiz);
+    }    
+    if (audioDescription.isPlay) {
+        play(audioDescription);
+    }
+    endPlay(audioDescription);
+    endPlay(audioQuiz);
     fillVariants();
     field.addEventListener('click', getAnswer);
     next.removeEventListener('click', nextQuestion);
     next.style.backgroundColor = '#909090';
+    next.style.cursor = 'default';
 
 }
 function showResult() {
-    alert(1);
+    clearTimeout(showResult);
+    let el = document.createElement('div');
+    let victoryText = document.createElement('div');
+    let victoryHeader = document.createElement('div');
+    let buttonNewGame = document.createElement('div');
+    let buttonReturn = document.createElement('div');
+    el.className = 'victoryWindow';
+    victoryHeader.className = 'victoryHeader';
+    victoryHeader.innerHTML = '<p>Поздравляем!!! <br> Вы выиграли!!!</p>';
+    let rightName = ['баллов','балл','балла','балла','балла','баллов','баллов','баллов','баллов','баллов'];
+
+    if (score == 30) victoryText.textContent = `Вы набрали максимальное количество баллов(30)!`
+    else victoryText.textContent = 'Вы набрали ' + score + ' ' + rightName[score % 10] +'. Попробуйте еще раз'
+    victoryText.className = 'victoryText';
+    buttonNewGame.className = 'buttonVictory';
+    buttonReturn.className = 'buttonVictory';
+    buttonNewGame.innerHTML = '<p>Новая игра</p>';
+    buttonReturn.innerHTML = '<p>Главная страница</p>';
+    body.append(el);
+    el.append(victoryHeader);
+    el.append(victoryText);
+    el.append(buttonNewGame);
+    el.append(buttonReturn);
+    buttonNewGame.addEventListener('click', restart);
+    buttonReturn.addEventListener('click', () => {document.location.href = 'index.html'})
+    initGame();
+}
+function restart() {
+    document.getElementsByClassName('victoryWindow')[0].remove();
+    initGame();
+    fillVariants();
+    field.addEventListener('click', getAnswer);
+}
+function play(player) {
+    let el = document.getElementById(player.playerName);
+    if (player.isPlay) {
+        player.isPlay = false;
+        el.style.backgroundImage = ' url("pause.svg")';
+        player.pause();
+    }
+    else {
+        player.isPlay = true;
+        el.style.backgroundImage = ' url("play.svg")';
+        el.style.backgroundColor = ' #505050';
+        player.play();
+    }
+}
+
+function endPlay(player) {
+    player.isPlay = false;
+    document.getElementById(player.playerName).style.backgroundColor = '#909090';
+    document.getElementById(player.playerName).style.backgroundImage = ' url("play.svg")';
 }
